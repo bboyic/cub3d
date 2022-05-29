a3d89eab314d3673fccdf1147bccd896e618e89e
zhig <realbboyic@gmail.com>
2022-05-29 18:28:46 +0300

    cube.h

--- mandatory/includes/cub3d.h
+++ mandatory/includes/cub3d.h
@@ -1,3 +1,5 @@
+#ifndef CUBE3D_H
+# define	CUBE3D_H
 
 typedef struct	s_data {
 	void	*img;
@@ -7,8 +9,28 @@ typedef struct	s_data {
 	int		endian;
 }				t_data;
 
-typedef struct s_list {
-	void *to_free;
-	struct s_list *next;
-}			t_list;
+typedef struct	s_obj {
+	int	x;
+	int	y;
+}	t_obj;
 
+typedef struct	s_player {
+	float	x;
+	float 	y;
+	float 	dx;
+	float 	dy;
+	float	a;
+	float	ra;
+	int		z;
+}	t_player;
+
+typedef struct	s_vars {
+	t_data	img;
+	t_data	textute;
+	t_data	hands;
+	void	*mlx;
+	void	*win;
+	t_player pl;
+}	t_vars;
+
+#endif
